5f88c8f40417cb59e548ba14d16cabfe063f5d9b
Géza Ahsendorf <75172915+DynamicGoose@users.noreply.github.com>
2023-07-05 09:10:24 +0200

    Update configuration.nix

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -302,6 +302,7 @@ in {
           bind = CTRL_ALT, T, exec, kitty
           bind = SUPER, A, exec, wofi
           bind = SUPER_ALT, L, exec, swaylock --screenshots --clock --indicator --effect-blur 8x8 --text-color ffffff --indicator-radius 200 --inside-color 00000000 --key-hl-color 00000000 --ring-color 00000000 --line-color 00000000 --separator-color 00000000 --text-ver-color ffffff --inside-ver-color 00000000 --ring-ver-color 00000000 --line-ver-color 00000000 --text-wrong-color cf4a4a --inside-wrong-color 00000000 --ring-wrong-color 00000000 --line-wrong-color 00000000 --text-clear-color 4acf4a --inside-clear-color 00000000 --ring-clear-color 00000000 --line-clear-color 00000000
+          bind = SUPER_ALT, P, exec, wlogout
           bind = ALT, comma, splitratio, -0.05
           bind = ALT, period, splitratio, +0.05
 
