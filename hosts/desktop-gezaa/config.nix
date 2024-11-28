38cb2e4b9875ea127060a0592847eea10379c70a
Géza Ahsendorf <gezaahs@gmail.com>
2024-11-28 15:41:59 +0100

    fix lact deamon

--- hosts/desktop-gezaa/config.nix
+++ hosts/desktop-gezaa/config.nix
@@ -23,10 +23,20 @@
     opencl.enable = true;
     initrd.enable = true;
   };
-  
+
+  # LACT (amdgpu control-panel)
   environment.systemPackages = with pkgs; [
     lact
   ];
+  systemd.services.lact = {
+    description = "AMDGPU Control Daemon";
+    after = ["multi-user.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    enable = true;
+  };
 
   # filesystem config and nix store on other device
   fileSystems = {
