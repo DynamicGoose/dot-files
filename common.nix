664edab56f9ba8aadc627dc1fd0b72d5434db0ce
Géza Ahsendorf <gezaahs@gmail.com>
2025-01-19 00:23:21 +0100

    finish redesign

--- common.nix
+++ common.nix
@@ -1163,44 +1163,6 @@
       };
     };
 
-    # Hypridle
-    services.hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          lock_cmd = "pidof hyprlock || hyprlock";
-          before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on";
-        };
-
-        listener = [
-          {
-            timeout = 150;
-            on-timeout = "brightnessctl -s set 10";
-            on-resume = "brightnessctl -r";
-          }
-          {
-            timeout = 150;
-            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-            on-resume = "brightnessctl -rd rgb:kbd_backlight";
-          }
-          {
-            timeout = 300;
-            on-timeout = "loginctl lock-session";
-          }
-          {
-            timeout = 380;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-          {
-            timeout = 1800;
-            on-timeout = "systemctl suspend";
-          }
-        ];
-      };
-    };
-
     # Waybar
     programs.waybar = {
       enable = true;
@@ -1310,7 +1272,7 @@
         }
 
         #input {
-          border-radius: 8px;
+        border-radius: 8px;
           color: #ffffff;
           background-color: #181818;
         }
--- modules/home-manager/standard.nix
+++ modules/home-manager/standard.nix
@@ -11,9 +11,43 @@
         monitor = [
           ", preferred, auto, 1"
         ];
+      };
+    };
 
-        exec-once = [
-          "hypridle"
+    # Hypridle
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+
+        listener = [
+          {
+            timeout = 150;
+            on-timeout = "brightnessctl -s set 10";
+            on-resume = "brightnessctl -r";
+          }
+          {
+            timeout = 150;
+            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+            on-resume = "brightnessctl -rd rgb:kbd_backlight";
+          }
+          {
+            timeout = 300;
+            on-timeout = "loginctl lock-session";
+          }
+          {
+            timeout = 380;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
         ];
       };
     };
