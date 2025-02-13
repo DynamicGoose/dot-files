a6aaf14883587312a5a88419a913f0c346539074
Géza Ahsendorf <gezaahs@gmail.com>
2025-02-13 10:21:37 +0100

    graphics

--- modules/graphics.nix
+++ modules/graphics.nix
@@ -1,14 +1,20 @@
-{ config, pkgs, lib, ...}: let
-  graphicsConfig = if (config.modules.graphics.type == "amd") then {
-    hardware.amdgpu = {
+{ config, pkgs, lib, ...}: {
+  options.modules.graphics.type = lib.mkOption {
+    type = lib.types.str;
+    default = "amd";
+  };
+
+  config = let
+    type = config.modules.graphics.type;
+  in {
+    # AMD
+    hardware.amdgpu = lib.mkIf (type == "amd") {
       opencl.enable = true;
       initrd.enable = true;
     };
-
-    # LACT (amdgpu control-panel)
-    environment.systemPackages = with pkgs; [ lact ];
-    # lactd service
-    systemd.services.lact = {
+    # Lact (amdgpu control-panel)
+    environment.systemPackages = lib.mkIf (type = "amd") [pkgs.lact];
+    systemd.services.lact = lib.mkIf (type == "amd") {
       description = "AMDGPU Control Daemon";
       after = ["multi-user.target"];
       wantedBy = ["multi-user.target"];
@@ -17,22 +23,16 @@
       };
       enable = true;
     };
-    # overclocking (kernel param)
-    boot.kernelParams = ["amdgpu.ppfeaturemask=0xffffffff"];
-  }; else if (config.modules.graphics.type == "intel") then {
-    hardware.graphics.extraPackages = with pkgs; [
-      intel-media-sdk
-      intel-media-driver
-      intel-vaapi-driver
-      libvdpau-va-gl
-    ];
-    environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
-  }; else {};
-in {
-  options.modules.graphics.type = lib.mkOption {
-    type = lib.types.str;
-    default = "amd";
-  };
+    # overclocking kernel param
+    boot.kernelParams = lib.mkIf (type == "amd") ["amdgpu.ppfeaturemask=0xffffffff"];
 
-  config = graphicsConfig;
+    # Intel
+    hardware.graphics.extraPackages = lib.mkIf (type == "intel") [
+      pkgs.intel-media-sdk
+      pkgs.intel-media-driver
+      pkgs.intel-vaapi-driver
+      pkgs.libvdpau-va-gl
+    ];
+    environment.sessionVariables = lib.mkIf (type == "intel") { LIBVA_DRIVER_NAME = "iHD"; };
+  };
 }
