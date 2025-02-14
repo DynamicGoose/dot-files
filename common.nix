b45b7079488810dfe92a2c9d7d85ff59c7730268
Géza Ahsendorf <gezaahs@gmail.com>
2025-02-14 14:38:51 +0100

    import modules

--- common.nix
+++ common.nix
@@ -1,3 +1,42 @@
 { config, pkgs, ... }: {
-  
+  imports = [
+    ./modules/niri.nix
+    ./modules/qt.nix
+    ./modules/users.nix
+    ./modules/graphics.nix
+    ./modules/security.nix
+    ./modules/power-management.nix
+    ./modules/locale.nix
+    ./modules/systemd.nix
+    ./modules/gtk.nix
+    ./modules/nix.nix
+    ./modules/networking.nix
+    ./modules/boot.nix
+    ./modules/virtualisation.nix
+    ./modules/cursor.nix
+    ./modules/nixpkgs.nix
+    ./modules/zsh.nix
+    ./modules/fonts.nix
+    ./modules/home-manager.nix
+    ./modules/services/audio.nix
+    ./modules/services/xserver.nix
+    ./modules/services/hypridle.nix
+    ./modules/services/syncthing.nix
+    ./modules/services/ssh.nix
+    ./modules/services/printing.nix
+    ./modules/services/dbus.nix
+    ./modules/services/lightdm.nix
+    ./modules/packages/packages.nix
+    ./modules/packages/nemo.nix
+    ./modules/packages/wofi-power-menu.nix
+    ./modules/packages/swaync.nix
+    ./modules/programs/alvr.nix
+    ./modules/programs/virt-manager.nix
+    ./modules/programs/steam.nix
+    ./modules/programs/wofi.nix
+    ./modules/programs/waybar.nix
+    ./modules/programs/hyprlock.nix
+    ./modules/programs/evince.nix
+    ./modules/programs/helix.nix
+  ];
 }
--- hosts/fw-gezaa/config.nix
+++ hosts/fw-gezaa/config.nix
@@ -5,13 +5,6 @@
   lib,
   ...
 }: {
-  imports = [
-    ../../modules/boot.nix
-    ../../modules/graphics.nix
-    ../../modules/power-management.nix
-    ../../modules/home-manager/standard.nix
-  ];
-
   modules.powerManagement.tlp.enable = true;
   services.fprintd.enable = true;
   networking.hostName = "fw-gezaa";
