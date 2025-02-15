cf2e1d8f723bbeb639e7637dea69cc2cf7be5267
Géza Ahsendorf <gezaahs@gmail.com>
2025-02-15 20:18:38 +0100

    revert update

--- hosts/tp-gezaa/config.nix
+++ hosts/tp-gezaa/config.nix
@@ -10,4 +10,10 @@
   };
   
   networking.hostName = "tp-gezaa";
+
+  home-manager.users.${config.modules.users.username} = { pkgs, ... }: {
+    programs.niri.settings = {
+      outputs."eDP-1".scale = 1.0;
+    };
+  };
 }
