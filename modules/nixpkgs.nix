8a3c3c30f2562b298d265dad310de3000d0e16dd
Géza Ahsendorf <gezaahs@gmail.com>
2025-02-15 21:23:07 +0100

    revert xwayland-satellite to 5.0

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,3 +1,20 @@
 { config, pkgs, ... }: {
-  nixpkgs.config = { allowUnfree = true; };
+  nixpkgs = {
+    config = { allowUnfree = true; };
+    overlays = [
+      # xwayland-satellite 0.5.1 breaks "Halo: The Master Chief Collection"
+      (final: prev: {
+        xwayland-satellite = prev.xwayland-satellite.overrideAttrs (_:rec {
+          version = "0.5";
+
+          src = fetchFromGitHub {
+            owner = "Supreeeme";
+            repo = "xwayland-satellite";
+            tag = "v${version}";
+            hash = "sha256-nq7bouXQXaaPPo/E+Jbq+wNHnatD4dY8OxSrRqzvy6s=";
+          };
+        });
+      })
+    ]
+  };
 }
