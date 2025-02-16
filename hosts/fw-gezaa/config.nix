67856c5fa54eb901887ade1362cb98e443fac456
Géza Ahsendorf <gezaahs@gmail.com>
2025-02-16 15:24:01 +0100

    hardware specific stuff for fw13

--- hosts/fw-gezaa/config.nix
+++ hosts/fw-gezaa/config.nix
@@ -4,10 +4,22 @@
   pkgs,
   ...
 }: {
-  modules.powerManagement.tlp.enable = true;
+  modules.powerManagement.ppd.enable = true;
+
   services.fprintd.enable = true;
   networking.hostName = "fw-gezaa";
 
+  # https://gitlab.freedesktop.org/drm/amd/-/issues/3647
+  boot.kernelParams = ["amdgpu.dcdebugmask=0x10"];
+  
+  # detect/manage display brightness
+  hardware.sensor.iio.enable = true;
+
+  services.udev.extraRules = ''
+    # Ethernet expansion card support
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8156", ATTR{power/autosuspend}="20"
+  '';
+    
   home-manager.users.${config.modules.users.username} = { pkgs, ... }: {
     programs.niri.settings = {
       outputs."eDP-1".scale = 1.0;
