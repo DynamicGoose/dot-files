a38aef9b494c7218b845b1bb9f000406c4e89250
Géza Ahsendorf <gezaahs@gmail.com>
2025-02-17 01:50:46 +0100

    sort imports

--- common.nix
+++ common.nix
@@ -1,43 +1,44 @@
 { config, pkgs, ... }: {
   imports = [
-    ./modules/users.nix
-    ./modules/niri.nix
-    ./modules/qt.nix
-    ./modules/graphics.nix
-    ./modules/security.nix
-    ./modules/power-management.nix
-    ./modules/locale.nix
-    ./modules/systemd.nix
-    ./modules/gtk.nix
-    ./modules/nix.nix
-    ./modules/networking.nix
     ./modules/boot.nix
-    ./modules/virtualisation.nix
     ./modules/cursor.nix
-    ./modules/nixpkgs.nix
-    ./modules/zsh.nix
+    ./modules/firmware.nix
     ./modules/fonts.nix
+    ./modules/graphics.nix
+    ./modules/gtk.nix
     ./modules/home-manager.nix
-    ./modules/services/audio.nix
-    ./modules/services/xserver.nix
-    ./modules/services/hypridle.nix
-    ./modules/services/syncthing.nix
-    ./modules/services/ssh.nix
-    ./modules/services/printing.nix
-    ./modules/services/dbus.nix
-    ./modules/services/lightdm.nix
-    ./modules/packages/packages.nix
+    ./modules/locale.nix
+    ./modules/networking.nix
+    ./modules/niri.nix
+    ./modules/nix.nix
+    ./modules/nixpkgs.nix
+    ./modules/power-management.nix
+    ./modules/qt.nix
+    ./modules/security.nix
+    ./modules/systemd.nix
+    ./modules/users.nix
+    ./modules/virtualisation.nix
+    ./modules/zsh.nix
     ./modules/packages/nemo.nix
-    ./modules/packages/wofi-power-menu.nix
+    ./modules/packages/packages.nix
     ./modules/packages/swaync.nix
+    ./modules/packages/wofi-power-menu.nix
     ./modules/programs/alvr.nix
-    ./modules/programs/virt-manager.nix
-    ./modules/programs/steam.nix
-    ./modules/programs/wofi.nix
-    ./modules/programs/waybar.nix
-    ./modules/programs/hyprlock.nix
     ./modules/programs/evince.nix
     ./modules/programs/helix.nix
+    ./modules/programs/hyprlock.nix
+    ./modules/programs/steam.nix
+    ./modules/programs/virt-manager.nix
+    ./modules/programs/waybar.nix
+    ./modules/programs/wofi.nix
+    ./modules/services/audio.nix
+    ./modules/services/dbus.nix
+    ./modules/services/hypridle.nix
+    ./modules/services/lightdm.nix
+    ./modules/services/printing.nix
+    ./modules/services/ssh.nix
+    ./modules/services/syncthing.nix
+    ./modules/services/xserver.nix
   ];
 
   system.stateVersion = "25.05";
