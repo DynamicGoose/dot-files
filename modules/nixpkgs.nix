9b927bb364d9db05220f530d6e0fc4a385f7043b
Géza Ahsendorf <gezaahs@gmail.com>
2025-03-23 12:18:56 +0100

    cleaner pkgs folder

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -3,12 +3,12 @@
     config = { allowUnfree = true; };
     overlays = [
       (final: prev: {
-        graphite-kde-theme-black = prev.callPackage ../packages/graphite-kde-theme-black.nix {
+        graphite-kde-theme-black = prev.callPackage ../pkgs/by-name/gr/graphite-kde-theme-black {
           kdeclarative = pkgs.libsForQt5.kdeclarative;
           plasma-framework = pkgs.libsForQt5.plasma-framework;
           plasma-workspace = pkgs.libsForQt5.plasma-workspace;
         };
-        bdp-fonts = prev.callPackage ../packages/bdp-fonts.nix { };
+        bdp-fonts = prev.callPackage ../pkgs/by-name/bd/bdp-fonts { };
       })
     ];
   };
