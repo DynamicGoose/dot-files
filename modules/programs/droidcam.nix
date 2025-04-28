f5e4e1fbfd34b9d9294c04ccf1fd4284e89d14f5
Géza Ahsendorf <gezaahs@gmail.com>
2025-04-28 22:24:30 +0200

    droidcam obs cam

--- modules/programs/droidcam.nix
+++ modules/programs/droidcam.nix
@@ -2,4 +2,9 @@
 {
   programs.droidcam.enable = true;
   programs.adb.enable = true;
+
+  # OBS cam
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 }
