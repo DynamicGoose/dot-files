25d80ecb95486fe6aa759ef8cae41258b1cd0172
Géza Ahsendorf <gezaahs@gmail.com>
2025-05-02 23:20:46 +0200

    add dev shell

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -59,6 +59,13 @@
     ];
     config = {
       common = {
+        default = [
+          "gnome"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Access" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
         "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
       };
     };
