f7a87f989531e4ac21e8c64bd5dece455dfcb850
Géza Ahsendorf <gezaahs@gmail.com>
2025-06-20 15:53:15 +0200

    zoxide: init

---
+++ modules/programs/zoxide.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  username,
+  ...
+}:
+{
+  home-manager.users.${username} =
+    { config, ... }:
+    {
+      programs.zoxide = {
+        enable = true;
+        enableNushellIntegration = true;
+      };
+
+      programs.nushell.shellAliases.cd = "z";
+    };
+}
