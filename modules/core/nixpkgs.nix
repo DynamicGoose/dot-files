0803894728a20f70377136b070be2c12f5de0330
Géza Ahsendorf <gezaahs@gmail.com>
2025-06-25 15:16:45 +0200

    nixpkgs: remove temporary linux-firmware fix

--- modules/core/nixpkgs.nix
+++ modules/core/nixpkgs.nix
@@ -15,16 +15,6 @@
           directory = ../../pkgs;
         }
       )
-      # Temporary Fix, remove after nixpkgs pr 418764
-      (final: prev: {
-        linux-firmware = prev.linux-firmware.overrideAttrs (old: rec {
-          version = "20250621";
-          src = prev.fetchzip {
-            url = "https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${version}.tar.xz";
-            hash = "sha256-0FrhgJQyCeRCa3s0vu8UOoN0ZgVCahTQsSH0o6G6hhY=";
-          };
-        });
-      })
     ];
   };
 }
