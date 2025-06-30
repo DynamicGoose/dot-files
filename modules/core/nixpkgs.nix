4a10f1e1b0f424da7675ca6861fa6d490f915b2d
Géza Ahsendorf <gezaahs@gmail.com>
2025-07-01 00:30:43 +0200

    obs-studio: temp fix for virtual cam

--- modules/core/nixpkgs.nix
+++ modules/core/nixpkgs.nix
@@ -15,6 +15,19 @@
           directory = ../../pkgs;
         }
       )
+      # temp obs virtual cam fix until https://nixpk.gs/pr-tracker.html?pr=421175
+      (final: prev: {
+        obs-studio = prev.obs-studio.overrideAttrs (old: rec {
+          version = "31.0.4";
+          src = prev.fetchFromGitHub {
+            owner = "obsproject";
+            repo = "obs-studio";
+            rev = version;
+            hash = "sha256-YxBPVXin8oJlo++oJogY1WMamIJmRqtSmKZDBsIZPU4=";
+            fetchSubmodules = true;
+          };
+        });
+      })
     ];
   };
 }
