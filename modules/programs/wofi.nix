2bf7bb11617e04f7b09d874fbea77aaceab0cbcd
Géza Ahsendorf <gezaahs@gmail.com>
2025-07-23 15:53:26 +0200

    wofi: fix styling

--- modules/programs/wofi.nix
+++ modules/programs/wofi.nix
@@ -24,17 +24,21 @@
         };
 
         style = ''
+          * {
+            border-radius: 8px;
+          }
+
           #window {
             margin: 0px;
             border: 2px solid #e0e0e0;
             border-radius: 10px;
-            background: rgba(15, 15, 15, 0.999);
+            background-color: rgba(15, 15, 15, 0.999);
             font-size: 16px;
             font-weight: Bold;
           }
 
           #input {
-          border-radius: 8px;
+            border-radius: 8px;
             color: #ffffff;
             background-color: #181818;
           }
@@ -50,30 +54,30 @@
             margin: 0px 8px 0px 8px;
             border: none;
             color: #ffffff;
+            font-weight: Bold;
           }
 
           #entry:selected {
           	background-color: #e0e0e0;
-          	font-weight: normal;
+          	font-weight: Bold;
           	border-radius: 8px;
           }
 
           #text:selected {
           	background-color: #e0e0e0;
           	color: #000000;
-          	font-weight: normal;
+          	font-weight: Bold;
           }
 
           #expander-box {
-            backgorund: #e0e0e0;
+            background-color: #0f0f0f;
             color: #000000;
-            font-weight: normal;
+            font-weight: Bold;
+            border-radius: 8px;
           }
 
-          #expander-box:selected {
-            background: #e0e0e0;
-            color: #000000;
-            font-weight: normal;
+          #expander-box #entry:selected {
+            background-color: #383838;
           }
         '';
       };
