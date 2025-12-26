156d14fa5a9eeba25fef3ddcb0ca7d89014fc156
Géza Ahsendorf <gezaahs@gmail.com>
2025-12-26 01:55:14 +0100

    qmk: enable udev rules

--- modules/core/firmware.nix
+++ modules/core/firmware.nix
@@ -1,6 +1,7 @@
 {
   # when using auto-generated hardware-configuration.nix, this will also enable microcode updates
   hardware.enableRedistributableFirmware = true;
+  hardware.keyboard.qmk.enable = true;
 
   services.fwupd.enable = true;
 }
