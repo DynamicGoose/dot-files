fd5117860bd07de23519775979fb9fe48416e8a8
Géza Ahsendorf <gezaahs@gmail.com>
2026-01-20 01:37:50 +0100

    wivrn: declerative config

--- modules/services/wivrn.nix
+++ modules/services/wivrn.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   services.wivrn = {
     enable = true;
@@ -5,5 +6,49 @@
     defaultRuntime = true;
     autoStart = true;
     steam.importOXRRuntimes = true;
+    config = {
+      enable = true;
+      json = {
+        scale = 0.6;
+        bitrate = 175000000;
+        encoder = "vulkan";
+        encoders = [
+          {
+            codec = "h265";
+            encoder = "vulkan";
+            height = 0.25;
+            width = 0.5;
+            offset_x = 0.5;
+            offset_y = 0.0;
+          }
+          {
+            codec = "h265";
+            encoder = "vulkan";
+            height = 0.3;
+            width = 0.5;
+            offset_x = 0.0;
+            offset_y = 0.7;
+          }
+          {
+            codec = "h265";
+            encoder = "vulkan";
+            height = 0.7;
+            width = 0.5;
+            offset_x = 0.0;
+            offset_y = 0.0;
+          }
+          {
+            codec = "h265";
+            encoder = "vulkan";
+            height = 0.75;
+            width = 0.5;
+            offset_x = 0.5;
+            offset_y = 0.25;
+          }
+        ];
+
+        application = [ pkgs.wayvr ];
+      };
+    };
   };
 }
