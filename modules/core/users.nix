6943dc88b67f3c1983a6b2c252a222c6cd45207d
Géza Ahsendorf <gezaahs@gmail.com>
2026-03-02 22:02:45 +0100

    immutable user config

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,26 +1,44 @@
 {
+  lib,
   pkgs,
   username,
   userDescription,
   ...
 }:
 {
+  # place password hash files in /home/${username}/secrets/
+
+  # admin user for system management
   users.defaultUserShell = pkgs.nushell;
+  users.mutableUsers = false;
+  users.users.admin = {
+    isNormalUser = true;
+    description = "System Administrator";
+    extraGroups = [ "wheel" ];
+    hashedPasswordFile = "/home/${username}/secrets/admin";
+    # openssh.authorizedKeys.keys = [];
+  };
+
+  # default user (doesn't have wheel)
   users.users.${username} = {
     isNormalUser = true;
     description = userDescription;
-    extraGroups = [
+    extraGroups = lib.mkForce [
       "networkmanager"
-      "wheel"
       "audio"
+      "video"
       "cdrom"
     ];
+    hashedPasswordFile = "/home/${username}/secrets/${username}";
   };
+
+  users.users.root.hashedPasswordFile = "/home/${username}/secrets/root";
 
   # set default passwords for vm builds
   virtualisation.vmVariant = {
     users.users = {
       root.password = "root";
+      admin.password = "admin";
       ${username}.password = username;
     };
   };
