1ba67a3c33c7c75601270bef225c0e8de9cd6456
Géza Ahsendorf <gezaahs@gmail.com>
2026-04-02 20:32:59 +0200

    gpu-screen-recorder: add disable option

--- modules/programs/gpu-screen-recorder.nix
+++ modules/programs/gpu-screen-recorder.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }:
 {
-  programs.gpu-screen-recorder.enable = true;
-  environment.systemPackages = [ pkgs.gpu-screen-recorder-gtk ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.modules.programs.gpu-screen-recorder.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config.programs.gpu-screen-recorder.enable = config.modules.programs.gpu-screen-recorder.enable;
+  config.environment.systemPackages = lib.mkIf (config.modules.programs.gpu-screen-recorder.enable) [
+    pkgs.gpu-screen-recorder-gtk
+  ];
 }
