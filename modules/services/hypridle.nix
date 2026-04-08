59ac7f8a523c574fd6d4ba3b4e4560758a944c18
Géza Ahsendorf <gezaahs@gmail.com>
2026-04-08 21:41:47 +0200

    hypridle: remove home-manager config

--- modules/services/hypridle.nix
+++ modules/services/hypridle.nix
@@ -10,65 +10,74 @@
 
   config =
     let
-      hypridleSettings =
+      hypridle-config = pkgs.writeText "hypridle.conf" (
         if (!config.modules.services.hypridle.desktop) then
-          {
-            general = {
-              lock_cmd = "goose-shell ipc call session lock";
-              before_sleep_cmd = "niri msg action power-off-monitors";
-              after_sleep_cmd = "loginctl lock-session && niri msg action power-on-monitors";
-            };
+          ''
+            general {
+              lock_cmd = goose-shell ipc call session lock
+              before_sleep_cmd = niri msg action power-off-monitors
+              after_sleep_cmd = loginctl lock-session && niri msg action power-on-monitors
+            }
 
-            listener = [
-              {
-                timeout = 150;
-                on-timeout = "brightnessctl -s set 10";
-                on-resume = "brightnessctl -r";
-              }
-              {
-                timeout = 150;
-                on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-                on-resume = "brightnessctl -rd rgb:kbd_backlight";
-              }
-              {
-                timeout = 300;
-                on-timeout = "loginctl lock-session";
-              }
-              {
-                timeout = 380;
-                on-timeout = "niri msg action power-off-monitors";
-                on-resume = "niri msg action power-on-monitors";
-              }
-              {
-                timeout = 1800;
-                on-timeout = "systemctl suspend";
-              }
-            ];
-          }
+            listener {
+              timeout = 150
+              on-timeout = brightnessctl -s set 10
+              on-resume = brightnessctl -r
+            }
+
+            listener {
+              timeout = 150
+              on-timeout = brightnessctl -sd rgb:kbd_backlight set 0
+              on-resume = brightnessctl -rd rgb:kbd_backlight
+            }
+
+            listener {
+              timeout = 300
+              on-timeout = loginctl lock-session
+            }
+
+            listener {
+              timeout = 380
+              on-timeout = niri msg action power-off-monitors
+              on-resume = niri msg action power-on-monitors
+            }
+
+            listener {
+              timeout = 1800
+              on-timeout = systemctl suspend
+            }
+          ''
         else
-          {
-            general = {
-              lock_cmd = "goose-shell ipc call session lock";
-              before_sleep_cmd = "niri msg action power-off-monitors";
-              after_sleep_cmd = "loginctl lock-session && niri msg action power-on-monitors";
-            };
-
-            listener = [ ];
-          };
+          ''
+            general {
+              lock_cmd = goose-shell ipc call session lock
+              before_sleep_cmd = niri msg action power-off-monitors
+              after_sleep_cmd = loginctl lock-session && niri msg action power-on-monitors
+            }
+          ''
+      );
     in
     {
       environment.systemPackages = lib.mkIf (!config.modules.services.hypridle.desktop) [
         pkgs.brightnessctl
       ];
 
-      systemd.user.services.hypridle.wantedBy = [ "graphical-session.target" ];
+      services.hypridle.enable = true;
 
-      home-manager.users.${username} =
-        { config, ... }:
+      # Copy hypridle config
+      systemd.services.hypridleConfig =
+        let
+          copy-config = pkgs.writeShellScript "copy-kvantum-config.sh" ''
+            ${pkgs.coreutils}/bin/mkdir -p /home/${username}/.config/hypr
+            ${pkgs.coreutils}/bin/ln -sf ${hypridle-config} /home/${username}/.config/hypr/hypridle.conf
+          '';
+        in
         {
-          services.hypridle = {
-            enable = true;
-            settings = hypridleSettings;
+          description = "Copy Hypridle user config";
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = copy-config;
           };
         };
     };
